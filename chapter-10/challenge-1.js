aa9eb0baf30e2b97d7574d73689b53dbb1b50fec
skmajumder <skmajumder.cse@gmail.com>
2023-03-18 11:47:04 +0600

    Passing Arguments

---
+++ chapter-10/challenge-1.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const poll = {
+    question: 'What is your favorite programming language?',
+    options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+    // This generates [0, 0, 0, 0]. More in the next section!
+    answers: new Array(4).fill(0),
+};
--- chapter-10/script.js
+++ chapter-10/script.js
@@ -4,29 +4,30 @@
  * @description Default Parameters
  */
 
-// const bookings = [];
-// const createBooking = function (
-//   flightNum,
-//   numPassengers = 1,
-//   price = 199 * numPassengers
-// ) {
-//   // ** Default Parameters (old ways, before ES6)
-//   //   numPassengers = numPassengers || 1;
-//   //   price = price || 199;
-//   const booking = {
-//     flightNum,
-//     numPassengers,
-//     price,
-//   };
-//   console.log(booking);
-//   bookings.push(booking);
-// };
+const bookings = []
 
-// createBooking('LM123');
-// createBooking('LM123', 5);
-// createBooking('LM123', 2, 500);
-// createBooking('LM123', undefined, 1000);
-// createBooking('LM123', null);
+const createBooking = function (flightNumber, numPassengers = 1, ticketPrice = 1000 * numPassengers) {
+    const booking = {
+        flightNumber, numPassengers, ticketPrice
+    }
+    console.log(booking)
+    bookings.push(booking)
+}
+createBooking('LH123')
+createBooking('LH123', 2)
+createBooking('LH123', 2, 1900)
+
+/**
+ * @description How Passing Arguments Works Value vs. Reference
+ */
+
+const flight = 'LM234';
+const passenger = {
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284,
+  DOB: '3/25/90',
+  city: 'Lisbon',
+};
 
 /**
  * @description How Passing Arguments Works Value vs. Reference
@@ -115,87 +116,87 @@
 
 // greet('Hello')('Alexer');
 
-const lufthansa = {
-  airline: 'Lufthansa',
-  iataCode: 'LH',
-  booking: [],
-  book(flightNum, passengerName) {
-    console.log(
-      `${passengerName} booked a sit on ${this.airline} flight ${this.iataCode}${flightNum}`
-    );
-    this.booking.push({
-      flight: `${this.iataCode}${flightNum}`,
-      passengerName,
-    });
-  },
-};
-
-const book = lufthansa.book;
-
-lufthansa.book(236, 'Jonas');
-lufthansa.book(236, 'Shuvo');
-console.log(lufthansa);
-
-const eurowings = {
-  airline: 'Eurowings',
-  iataCode: 'EW',
-  booking: [],
-};
-
-book.call(eurowings, 23, 'Sarah William');
-
-const swiss = {
-  airline: 'Swiss Air Lines',
-  iataCode: 'LX',
-  booking: [],
-};
-
-book.call(swiss, 33, 'Sara Cooper');
-book.call(swiss, 22, 'Mary Cooper');
-
-// Apply Method
-
-const flightData = [56, 'David Cooper'];
-book.apply(swiss, flightData);
-
-book.call(swiss, ...flightData);
-console.log(swiss);
-
-const bookEW = book.bind(eurowings);
-bookEW(22, 'DB Cooper');
-
-// ** Partial Application
-
-const bookEW23 = book.bind(eurowings, 23);
-bookEW23('Eddie Redmayne');
-bookEW23('john Scamander');
-bookEW23('Martha Scamander');
-console.log(eurowings);
-
-// With event listeners
-lufthansa.planes = 300;
-lufthansa.buyPlane = function () {
-  this.planes++;
-  console.log(this.planes);
-};
-
-document
-  .getElementById('btn-buy')
-  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
-
-// ** Partial Application
-
-const addTax = (rate, value) => value + value * rate;
-// console.log(addTax(0.1, 200));
-
-const addVAT = addTax.bind(null, 0.23);
-// addVAT = value => value + value * .23
-console.log(addVAT(100));
-console.log(addVAT(200));
-console.log(addVAT(3500));
-
-const addTaxRate = rate => value => {
-  return value + value * rate;
-};
-const addVAT2 = addTaxRate(0.23);
-console.log(addVAT2(100));
+// const lufthansa = {
+//     airline: 'Lufthansa',
+//     iataCode: 'LH',
+//     booking: [],
+//     book(flightNum, passengerName) {
+//         console.log(
+//             `${passengerName} booked a sit on ${this.airline} flight ${this.iataCode}${flightNum}`
+//         );
+//         this.booking.push({
+//             flight: `${this.iataCode}${flightNum}`,
+//             passengerName,
+//         });
+//     },
+// };
+//
+// const book = lufthansa.book;
+//
+// lufthansa.book(236, 'Jonas');
+// lufthansa.book(236, 'Shuvo');
+// console.log(lufthansa);
+//
+// const eurowings = {
+//     airline: 'Eurowings',
+//     iataCode: 'EW',
+//     booking: [],
+// };
+//
+// book.call(eurowings, 23, 'Sarah William');
+//
+// const swiss = {
+//     airline: 'Swiss Air Lines',
+//     iataCode: 'LX',
+//     booking: [],
+// };
+//
+// book.call(swiss, 33, 'Sara Cooper');
+// book.call(swiss, 22, 'Mary Cooper');
+//
+// // Apply Method
+//
+// const flightData = [56, 'David Cooper'];
+// book.apply(swiss, flightData);
+//
+// book.call(swiss, ...flightData);
+// console.log(swiss);
+//
+// const bookEW = book.bind(eurowings);
+// bookEW(22, 'DB Cooper');
+//
+// // ** Partial Application
+//
+// const bookEW23 = book.bind(eurowings, 23);
+// bookEW23('Eddie Redmayne');
+// bookEW23('john Scamander');
+// bookEW23('Martha Scamander');
+// console.log(eurowings);
+//
+// // With event listeners
+// lufthansa.planes = 300;
+// lufthansa.buyPlane = function () {
+//     this.planes++;
+//     console.log(this.planes);
+// };
+//
+// document
+//     .getElementById('btn-buy')
+//     .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+//
+// // ** Partial Application
+//
+// const addTax = (rate, value) => value + value * rate;
+// // console.log(addTax(0.1, 200));
+//
+// const addVAT = addTax.bind(null, 0.23);
+// // addVAT = value => value + value * .23
+// console.log(addVAT(100));
+// console.log(addVAT(200));
+// console.log(addVAT(3500));
+//
+// const addTaxRate = rate => value => {
+//     return value + value * rate;
+// };
+// const addVAT2 = addTaxRate(0.23);
+// console.log(addVAT2(100));
